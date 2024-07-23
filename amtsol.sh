a8ae847653d8952c79e66c4cb52acd6ea1ec991c
Jerry Abramson <21062491+jerryabramson@users.noreply.github.com>
2024-07-23 13:30:23 -0400

    sample script refinements

--- amtsol.sh
+++ amtsol.sh
@@ -10,5 +10,9 @@ fi
 # Example using Mutual TLS
 CLIENT_CERT="$HOME/Critical/SSL_CERTS/AMT_CLIENT_AND_CA/amtclient.cert"
 CLIENT_KEY="$HOME/Critical/SSL_CERTS/AMT_CLIENT_AND_CA/amtclient.key"
-amtterm  $V -A  -c "$CLIENT_CERT" -k "$CLIENT_KEY" $@
+#amtterm  $V -A  -c "$CLIENT_CERT" -k "$CLIENT_KEY" $@
+
+# Example using custom root ca
+CERT_CA="/Users/jerry/Critical/SSL_CERTS/AMT_ALL_3_2024/AMT4/full_chain.pem"
+amtterm  $V -C "$CERT_CA"  -c "$CLIENT_CERT" -k "$CLIENT_KEY" $@
 
